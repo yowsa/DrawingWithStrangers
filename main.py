08a96d8a6cf51bc000b4ee0abb7a182d88e84d38
yowsa <josefin@fundin.com>
2019-06-21 16:37:41 +0100

    moved allt he javascript over to the seperate js file and sorted the jquery ready function to make it work. Worked seperatly for the first version of the backend class that will receive data from the front end, this class will receive the positions for each line that is drawn and save it in json format

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+mydict = {'key' : 'value', 'hey' : 'yo'}
+
 @app.route('/hello', methods=['GET', 'POST'])
 def hello_world():
     result = "results"
@@ -11,6 +13,37 @@ def hello_world():
 
 
 
+@app.route('/convertor', methods=['POST'])
+def convert():
+    #result = "results"
+    value = request.json['name']
+    mydict['hej'] = str(value)
+    return jsonify(mydict)
+
+
+
+
+print(mydict)
+
+class DrawReceiver:
+    def __init__(self, your_route):
+      self.your_route = your_route
+      @app.route(your_route, methods=['POST'])
+      
+      def jsonConvertor():
+        value = request.json['name']
+        mydict['hej'] = str(value)
+        return jsonify(mydict)
+
+draws = DrawReceiver('/convert')
+
+
+
+# Class som tar emot ritade streck, kan skicka ut ritade streck, ta bort gamla streck + test
+#farg, tjocklek och position {line : [3,4,5,6,7,8,8,9,0,]}
+
+
+# funtion som gor om ovan till json
 
 """
 @app.route('/second')
--- static/index.js
+++ static/index.js
@@ -1,5 +1,5 @@
 
-/*
+
 $.ajax({
 	datatype: "json",
 	url: "/hello",
@@ -8,7 +8,123 @@ $.ajax({
 		alert(JSON.stringify(x))
 	}
 })
+
+
+function sendPosition() {
+	$.ajax({
+	datatype: "json",
+	url: "/hello",
+	data: {"name" : "josefin"},
+	success: function(x){
+		alert(JSON.stringify(x))
+	}
+})
+
+}
+
+
+$(function(){
+
+    function showStrokeSize() {
+        document.getElementById("strokeSize").innerText = input.line.lineWidth
+
+    }
+
+
+//{widh:5, color:blue, position;[5,6,7,7,5,4,4{3;4},{5:7}]}
+
+
+// class som ritar
+class CanvasInput {
+
+    pos = {x: 0, y:0};
+
+    setCursorPosition(e) {
+        this.e = e
+        this.pos.x = this.e.clientX;
+        this.pos.y = this.e.clientY;
+    }
+
+    constructor(canvasID){
+        this.canvasID = canvasID
+        this.canvas = document.getElementById(this.canvasID);
+        this.line = this.canvas.getContext("2d");
+        this.line.lineWidth = 5;
+        this.line.strokeStyle = "#ff0000"
+    }
+
+    lineColor(color) {
+        this.color = color;
+        this.line.strokeStyle = this.color;
+    }
+
+    increaseStroke() {
+        if (this.line.lineWidth < 20){
+            this.line.lineWidth += 1
+        } 
+        showStrokeSize();
+    }
+
+    decreaseStroke() {
+        if (this.line.lineWidth > 1){
+            this.line.lineWidth -= 1
+        }
+        showStrokeSize();
+    }
+
+
+    setBeginLinePosition() {
+        this.line.beginPath();
+        this.line.moveTo(this.pos.x, this.pos.y);
+
+    }
+
+    drawLine(){
+        this.line.lineTo(this.pos.x, this.pos.y);
+        this.line.stroke();
+    }
+
+    startDrawing(){
+        this.myvar = setInterval(this.drawLine.bind(this), 1000);
+    }
+
+    finishDrawing(){
+        clearInterval(this.myvar);
+    }
+
+/*
+    this.canvas.addEventListener("mousemove", setCursorPosition);
+    this.canvas.addEventListener("mousedown", setBeginLinePosition);
+    this.canvas.addEventListener("mousedown", startDrawing);
+    this.canvas.addEventListener("mouseup", input.finishDrawing);
 */
+
+}
+
+input = new CanvasInput("mycanvas");
+showStrokeSize();
+
+
+document.addEventListener("mousemove", input.setCursorPosition.bind(input));
+document.addEventListener("mousedown", input.setBeginLinePosition.bind(input));
+document.addEventListener("mousedown", input.startDrawing.bind(input));
+document.addEventListener("mouseup", input.finishDrawing.bind(input));
+
+
+
+class ServerTalker {
+
+    lineCompleted() {
+
+    }
+
+}
+
+
+});
+
+// class som skickar och tar emot info från backend
+
 
 
 
